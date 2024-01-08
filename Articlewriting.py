0f3991a63beabdd10ac89826b95568a86cecb932
RaeesKMKM <155948646+RaeesKMKM@users.noreply.github.com>
2024-01-08 17:34:28 +0530

    Update Articlewriting.py

--- Articlewriting.py
+++ Articlewriting.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 clt = OpenAI(
     # defaults to os.environ.get("OPENAI_API_KEY")
-    api_key=‘Your-Key’
+    api_key='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')
 
 def main():
     st.title("Article Writer")
